ed0ea241948f007b765552139f42ee0bf17315ac
rvermote <robin.vermote@telenet.be>
2023-06-15 15:25:28 +0900

    finalized pipeline

--- processVideos.py
+++ processVideos.py
@@ -8,11 +8,12 @@ import json
 from pathlib import Path
 from dotenv import load_dotenv
 import pinecone
+from collections import defaultdict
 from sentence_transformers import SentenceTransformer
 
 load_dotenv()
 
-#Setting constants, definitions and loading in models
+#Setting constants, definitions and loading in models and data
 videoFolder = Path(os.path.dirname(os.path.abspath('__file__'))).parent.parent / "PhysicsVideos"
 model = whisper.load_model("small")
 
@@ -23,6 +24,12 @@ pinecone.init(api_key=os.getenv("KEY"),
                 environment=os.getenv("ENV"))
 index = pinecone.Index("youtube-search")
 
+video_meta=defaultdict(dict)
+with open("video_meta.txt","r") as f:
+    for line in f:
+        data = line.strip().split("\\")
+        video_meta[data[0]] = {"title":data[1],"url":data[2]}
+
 BATCH_SIZE = 64
 SENTENCE_TRANSFORMER_INPUT_TOKEN_LENGTH=128
 NUM_OF_VIDEOS=136
@@ -31,10 +38,10 @@ NUM_OF_VIDEOS=136
 
 for i in range(1,NUM_OF_VIDEOS+1):
 
-    print("Started processing video "+str(i))
+    vidIndex = str(i).zfill(3)
+    print("Started processing video "+vidIndex)
 
-    vidIndex = i.zfill(3,"0")
-    result = model.transcribe(whisper.load_audio(videoFolder / "001.webm"))
+    result = model.transcribe(whisper.load_audio(videoFolder / (vidIndex+".webm")))
 
     print("Finished Speech-To-Text video "+str(i))
 
@@ -66,13 +73,13 @@ for i in range(1,NUM_OF_VIDEOS+1):
     Total_package,filename = [],"pinecone_upserts.json"
 
     #process single list of words and timestamps into batches of token inputs
-    for i in range(0,len(tokenInput),BATCH_SIZE):
-        batch = tokenInput[i:min(i+BATCH_SIZE,len(tokenInput))]
+    for idx in range(0,len(tokenInput),BATCH_SIZE):
+        batch = tokenInput[idx:min(idx+BATCH_SIZE,len(tokenInput))]
 
-        ids = list(map(lambda x:str(vidIndex)+"-"+str(i)+"-"+str(x["start"]), batch))
+        ids = list(map(lambda x:str(vidIndex)+"-"+str(idx)+"-"+str(x["start"]), batch))
         #get embeddings using the sentence transformer
         embeddings = retriever.encode(list(map(lambda x:x["text"],batch))).tolist()
-        metadata = list(map(lambda x:{"text":x["text"],"video_id":vidIndex,"start":int(x["start"]),"end":int(x["end"])},batch))
+        metadata = list(map(lambda x:{"text":x["text"],"title":video_meta[vidIndex]["title"],"url":video_meta[vidIndex]["url"],"start":int(x["start"]),"end":int(x["end"])},batch))
 
         package = list(zip(ids,embeddings,metadata))
         package = list(map(lambda x:{'id':x[0],'values':x[1],'metadata':x[2]},package))
@@ -81,7 +88,7 @@ for i in range(1,NUM_OF_VIDEOS+1):
         index.upsert(vectors=package)
         Total_package.append(package)
     
-    print("Succesfully upserted video "+str(i)+" into pinecone index")
+    print("Succesfully upserted video "+vidIndex+" into pinecone index")
 
     with open(filename, 'w') as f:
         json.dump(Total_package, f, indent=4)
